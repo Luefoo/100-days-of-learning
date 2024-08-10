b16d81b53dee9e23f89f797660aedf6228086edc
luefoo <migsouzaofc@gmail.com>
2024-08-10 15:11:14 -0300

    Chegando la

---
+++ Dia 10/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,53 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros(){
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros(){
+        System.out.println(21 - 2);
+    }
+
+    public void multiplicaDoisNumeros(int num1, int num2){
+        System.out.println(num1 * num2);
+
+    }
+
+    public double divideDoisNumeros(double num1, double num2){
+
+        if (num2 == 0){
+            return 0;
+        }
+        return (int) (num1/num2);
+    }
+
+    public double divideDoisNumeros02(double num1, double num2) {
+
+        if (num2 == 0) {
+            return 0;
+        } else {
+            return (int) (num1 / num2);
+        }
+    }
+
+    public double divideDoisNumeros03(double num1, double num2) {
+
+        if (num2 != 0) {
+            return num1/num2;
+        } else {
+            return 0;
+        }
+    }
+
+    public void imprimeDivisaoDoisNumeros(double num1, double num2){
+        if (num2 == 0){
+            System.out.println("Toda divisao por 0 e = 0");
+        }else {
+            System.out.println(num1/num2);
+        }
+
+    }
+
+}
---
+++ Dia 10/README.md
@@ -0,0 +1,16 @@
+# 100-days-of-learning
+
+08/03
+Hoje continuei a estudar POO.
+
+Aulas assistidas:'
+
+Aula 41: Orientação Objetos - Coesão;
+Aula 42: Orientação Objetos - Exercício Classes;
+Aula 43: Orientação Objetos - Referência de objetos;
+Aula 44: Orientação Objetos - Métodos pt 01;
+Aula 45: Orientação Objetos - Métodos pt 02 - Parâmetros;
+Aula 46: Orientação Objetos - Métodos pt 03 - Retorno pt 01;
+Aula 47: Orientação Objetos - Métodos pt 04 - Retorno pt 02.
+
+Quase chegando na materia do curso tecnico.
