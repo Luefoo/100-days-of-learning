bc2f69c284cc4370cce224889eefd2bd0971c69a
luefoo <migsouzaofc@gmail.com>
2024-09-03 15:03:43 -0300

    Estudando

---
+++ Dia 24/Npolimorfismo/test/Test01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class Test01 {
+    public static void main(String[] args) {
+        Computador computador = new Computador("NUC10i7", 11000);
+        Tomate tomate = new Tomate("Tomate Siciliano", 10);
+        CalculadoraImposto.calcularImpostoComputador(computador);
+        System.out.println("-------------------------------------");
+        CalculadoraImposto.calcularImpostoTomate(tomate);
+
+    }
+}
---
+++ Dia 24/README.md
@@ -0,0 +1,5 @@
+# 100-days-of-learning
+Aulas assistidas:
+Aula 88 - Orientação Objetos - Interfaces pt 02 - Implementando múltiplas interfaces
+Aula 89 - Orientação Objetos - Interfaces pt 03 - Atributos e métodos estáticos
+Aula 90 - Orientação Objetos - Polimorfismo pt 01 - Introdução
